816ef2b077f411e46d386da22b3bc3bc90bf7232
4015a14 <4015a14@limon.ece.uprm.edu>
2017-11-15 12:46:14 -0400

    1.Change the frame's size to 200x200. 2.Change the frame's location to the center. 3.Change the frame's title to "Window".

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Window");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
 	}
 }
